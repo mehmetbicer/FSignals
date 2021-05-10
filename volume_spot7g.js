7578b8f1961ca6d786d8498914bf04c93b71bfa0
mehmetbicer <mehmetbicer78@hotmail.com>
2021-05-10 20:11:03 +0300

    V3

--- volume_spot7g.js
+++ volume_spot7g.js
@@ -10,8 +10,8 @@ const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
 //---------PARAMETRE ALANI---------------
-var param_ ="1m";
-var zamanlama_ = 15000;
+var param_ ="5m";
+var zamanlama_ = 10000;
 var __recvWindow = 50000;
 //---------OTOMATIK ALANLAR--------------
 var logs_ ="";
@@ -49,7 +49,7 @@ function ParaBul(){
     var request = require('request');
     var options = {
         'method': 'GET',
-        'url': 'https://api.binance.com/api/v3/exchangeInfo',
+        'url': 'https://fapi.binance.com/fapi/v1/exchangeInfo',
     };
 
     try {
@@ -60,21 +60,17 @@ function ParaBul(){
                 var paramm_ = params_.symbols;
                
                 paramm_.forEach(element => {
-                    if (element.quoteAsset=="USDT" && element.status=="TRADING" && element.isMarginTradingAllowed == true ){
-                        //console.log(element.symbol);
-                        
-                        var inx_ = 0;
-                        var inx1_ = 0;
-                       
-                        inx_ = element.symbol.indexOf("DOWN");
-                        inx1_ = element.symbol.indexOf("UP");
-                       
-                        
-                        if(inx_ == -1 && inx1_ == -1) {
-                            //console.log(element.symbol);
-                            Kontrols(element.symbol);
+                    if (element.quoteAsset=="USDT" && element.status=="TRADING" && element.contractType == "PERPETUAL" ){
+                     
+                        if(element.symbol != "DEFIUSDT"){
+                            if(element.symbol != "1000SHIBUSDT"){
+                                Kontrols(element.symbol);
+                            }
+                            
                         }
-                        //paralar_.push(element.symbol);
+
+                          
+                      
                     }
 
                 });
@@ -91,36 +87,40 @@ function ParaBul(){
     //zamanlama_=5000;
 }
 
-
+//'url': 'https://api.binance.com/api/v3/klines?' + deger_ ,
 
 function Kontrols(item) {
-    var limit_ = 8;
+    var limit_ = 7;
     var t_zaman = Date.now();
     var deger_ = "symbol="+ item  + "&interval=" + param_  + "&limit="  + limit_ + "&endTime=" + t_zaman 
     var request = require('request');
     var options = {
         'method': 'GET',
-        'url': 'https://api.binance.com/api/v3/klines?' + deger_ ,
+        'url': 'https://fapi.binance.com/fapi/v1/klines?' + deger_ ,
     };
+
+   
 
     try {
         request(options, function (error, response) {
             if (error == null || error == "undefined"){
                 var params_  = JSON.parse(response.body);
-                
+             
                 var takip_time = (Date.now());
                 var son_bar = params_[params_.length-1]; 
                 var son_bar_close =0;
                 var son_bar_time=0;
                 var son_bar_zaman =0;
                 var son_bar_vol =0;
+                var son_bar_vol_buy=0;
                 son_bar_close = son_bar[4];
                 son_bar_open = son_bar[1];
                 son_bar_time = son_bar[0];
                 son_bar_vol = son_bar[5];
-                
+                son_bar_vol_buy = son_bar[9];
                 
                 var toplam_hacim = 0;
+                var toplam_hacim_buy = 0;
                 var en_yuksek =0;
 
                 var sayac_ =0;
@@ -131,7 +131,7 @@ function Kontrols(item) {
                     var bar_open = parseFloat(first_bar_[1]);
                     var bar_close = parseFloat(first_bar_[4]);
                     var bar_volume = parseFloat(first_bar_[5]);
-
+                    var bar_volume_buy = parseFloat(first_bar_[9]);
 
                     sayac_ += 1 ;
                     //console.log(item + " : " +  first_bar_[0] + " syc: " + sayac_ );
@@ -139,6 +139,8 @@ function Kontrols(item) {
                     if(sayac_ < 8){
 
                         toplam_hacim += parseFloat(bar_volume);
+                        toplam_hacim_buy += parseFloat(bar_volume_buy);
+
                         if( parseFloat(bar_close) > parseFloat(en_yuksek)  ){
                             en_yuksek = parseFloat(bar_close);
                             //     console.log(item + " " + en_yuksek);
@@ -146,19 +148,14 @@ function Kontrols(item) {
 
                     }
 
-                   
-
-
-                   
-
                 });
 
 		
                 var oran__ =0;
-
-                
-
+                var hac_oran =0;
+                hac_oran =( parseFloat(toplam_hacim_buy) / parseFloat(toplam_hacim)) * 100
 				
+               // console.log(item + " ORAN : " + oran__);
                 if(  parseFloat(son_bar_vol) > parseFloat(toplam_hacim / 7) ){
                     durum_ ="HACIM GECTİ.";
 
@@ -167,22 +164,17 @@ function Kontrols(item) {
                     oran__ = parseFloat(oran__ - 100 );
                 }
 
-                if(parseFloat(oran__) > 24 && parseFloat(son_bar_close) > parseFloat(en_yuksek) && parseFloat(son_bar_close) > parseFloat(son_bar_open) ){
-
+                if(parseFloat(oran__) > 24 &&  parseFloat(hac_oran) > 49 && parseFloat(son_bar_close) > parseFloat(son_bar_open) ){
                         // console.log(item + ";" + parseFloat(son_bar_close).toFixed(8) + ";" + parseFloat(en_yuksek).toFixed(8) + ";" + parseFloat(son_bar_vol).toFixed(0) + ";" + parseFloat(toplam_hacim / 7).toFixed(0) + ";" +  parseFloat(toplam_hacim).toFixed(0) );
-                    
                         //console.log(item + " " + parseFloat(oran__).toFixed(2) );
                         //console.log("-----------------------------------------------------------");
                         var status_ = "";
-
                         if(parseFloat(son_bar_close) > parseFloat(son_bar_open) ){
                             status_ ="thumbs-up";
                         }else
                         { status_ ="thumbs-down";
                         }
-
                         var renk_ = "";
-                        
                         if(parseFloat(oran__).toFixed(2)<100){ renk_ = "btn-default"}
                         if(parseFloat(oran__).toFixed(2)>100){ renk_ = "btn-primary"}
                         if(parseFloat(oran__).toFixed(2)>200){ renk_ = "btn-info"}
@@ -194,7 +186,6 @@ function Kontrols(item) {
 
                         var feed = {name: item , hacim: parseFloat(g_hacim) , renk: renk_ , status1:status_};
                         json_data_.push(feed);
-
 					
                   
                 }
